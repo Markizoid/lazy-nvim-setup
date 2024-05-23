8eac7d247202a6d43cd3479425a01bf541ab5916
Marat <bunmipo@gmail.com>
2024-05-23 16:53:32 +0300

    [feat] added bqf plugin for QFL

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -24,11 +24,15 @@ return {
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      use_diagnostic_signs = true,
+      auto_jump = {"lsp_definitions"},
+    },
   },
 
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  { "folke/trouble.nvim",      enabled = false },
 
   -- add symbols-outline
   {
